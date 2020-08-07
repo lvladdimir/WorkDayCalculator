379c20f179b12e9ff2c9a56c31faa5bbda2016c5
lvladdimir <61430774+lvladdimir@users.noreply.github.com>
2020-08-07 16:27:37 +0300

    Add files via upload

---
+++ TestTask Template/CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        private TimeSpan Oneday = new TimeSpan(1, 0, 0, 0);
+
+        // if there are any interseptions of weekends
+        private List<WeekEnd> WeekendsMegre(WeekEnd[] weekEnds)
+        {
+            if (weekEnds == null) { return new List<WeekEnd>(); }
+            List<WeekEnd> list = new List<WeekEnd>();
+            WeekEnd w=null;
+            foreach( WeekEnd weekEnd in weekEnds)
+            {
+                if (w is null) { w = weekEnd; continue; }
+                if (weekEnd.StartDate - this.Oneday > w.EndDate)  { list.Add(w); w = weekEnd; }
+                else { w.EndDate = w.EndDate > weekEnd.EndDate ? w.EndDate : weekEnd.EndDate; }
+            }
+            list.Add(w);
+            return list;
+        }
+
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            // sort weekEnds by StartDate
+            Array.Sort(weekEnds, delegate (WeekEnd w1, WeekEnd w2)
+            {
+                return w1.StartDate.CompareTo(w2.StartDate);
+            });
+
+            List<WeekEnd> WeekEndList = WeekendsMegre(weekEnds);
+
+            DateTime endDate = startDate.AddDays(dayCount-1);
+
+            //check weekends
+            DateTime min, max;
+
+
+            foreach (WeekEnd weekEnd in WeekEndList)
+            {
+                if (weekEnd.StartDate > endDate) { return endDate; }
+
+                
+                if (weekEnd.StartDate <= endDate)
+                {
+                    //min = weekEnd.EndDate > endDate ? endDate : weekEnd.EndDate;
+                    max = weekEnd.StartDate < startDate ? startDate : weekEnd.StartDate;
+                    endDate += (weekEnd.EndDate - max).Add(this.Oneday); }
+            }
+
+            return endDate;
+        }
+    }
+}
---
+++ TestTask Template/CSharpTest/WorkDayCalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class WorkDayCalculatorTests
+    {       
+
+        [TestMethod]
+        public void TestNoWeekEnd()
+        {
+            WeekEnd[] weekends = new WeekEnd[3]
+         {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)),
+                new WeekEnd(new DateTime(2018, 4, 29), new DateTime(2019, 4, 29)),
+                new WeekEnd(new DateTime(2017, 4, 29), new DateTime(2017, 4, 29))
+         };
+
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.AreEqual(startDate.AddDays(count-1), result);
+        }
+
+        [TestMethod]
+        public void TestNormalPath()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25))
+            }; 
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }
+
+        [TestMethod]
+        public void TestWeekendAfterEnd()
+        {
+            DateTime startDate = new DateTime(2017, 4, 10);
+            int count = 15;
+            WeekEnd[] weekends = new WeekEnd[4]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)),
+                new WeekEnd(new DateTime(2017, 4, 15), new DateTime(2017, 4, 15)),
+                new WeekEnd(new DateTime(2017, 4, 28), new DateTime(2017, 4, 30)),
+                new WeekEnd(new DateTime(2017, 4, 26), new DateTime(2017, 4, 29))
+            };
+            
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 5, 3)));
+        }
+    }
+}
